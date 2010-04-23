be5aff2ee56c8f1ca0bc3098c49d5ae1801827d1
Mike.Popoloski <Mike.Popoloski@7c5bfe88-ddf6-11de-b357-fb893c5a43fa>
2010-04-23 14:55:23 +0000

    Starting over with a C# library instead.

---
+++ SlimMesh2/SlimMesh/MaterialParameter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlimMesh
+{
+    public enum ParameterType
+    {
+        Float,
+        Bool,
+        Int,
+        Matrix,
+        MatrixArray,
+        Vector2,
+        Vector3,
+        Vector4,
+        VectorArray,
+        FloatArray,
+        BoolArray,
+        IntArray
+    }
+
+    public class MaterialParameter
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public ParameterType DataType
+        {
+            get;
+            set;
+        }
+
+        public object Value
+        {
+            get;
+            set;
+        }
+
+        public MaterialParameter(string name, ParameterType dataType)
+            : this(name, dataType, null)
+        {
+        }
+
+        public MaterialParameter(string name, ParameterType dataType, object value)
+        {
+            Name = name;
+            DataType = dataType;
+            Value = value;
+        }
+    }
+}
---
+++ SlimMesh2/SlimMesh/Renderers/ModelRenderer10.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SlimDX.Direct3D10;
+using Buffer = SlimDX.Direct3D10.Buffer;
+using SlimDX;
+
+namespace SlimMesh.Renderers
+{
+    public class ModelRenderer10 : IDisposable
+    {
+        struct RenderData
+        {
+            public Buffer VertexBuffer;
+            public Buffer IndexBuffer;
+            public InputElement[] InputElements;
+        }
+
+        Device device;
+        Dictionary<Model, RenderData> renderData = new Dictionary<Model, RenderData>();
+
+        public ModelRenderer10(Device device)
+        {
+            this.device = device;
+        }
+
+        public void Prepare(Model model)
+        {
+            var renderData = new RenderData();
+
+            using (var data = new DataStream(model.Indices.ToArray(), true, false))
+                renderData.IndexBuffer = new Buffer(device, data, model.Indices.Count * sizeof(int), ResourceUsage.Default, BindFlags.IndexBuffer, CpuAccessFlags.None, ResourceOptionFlags.None);
+            using (var data = new DataStream(model.VertexData.Positions.ToArray(), true, false))
+                renderData.VertexBuffer = new Buffer(device, data, model.VertexData.Positions.Count * Vector3.SizeInBytes, ResourceUsage.Default, BindFlags.VertexBuffer, CpuAccessFlags.None, ResourceOptionFlags.None);
+
+
+        }
+
+        public void Render(Model model)
+        {
+            if (!renderData.ContainsKey(model))
+                Prepare(model);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+            }
+        }
+    }
+}
